1e202fd6a641197e828698843c4a0fc5b73c7c1a
Roland Pirklbauer <pirklbauer@walter-group.com>
2025-01-23 15:57:29 +0100

    Rollenauswertungen IDM

---
+++ idm/misc/auswerten-r10.ps1
@@ -0,0 +1,90 @@
+$fn = 'C:\temp\2025q1\r10-4.json'
+$j = get-content C:\temp\2025q1\r10-4.json | ConvertFrom-Json
+
+# Resourcen (.res.resources[])
+# (id) - technische Id der Resource
+# name - Name der Resource (ACHTUNG: Bei AD-Gruppen ADS-INT_Group)
+# (description) - description (der Ressource)
+# mappingDescription - Beschreibung der Resourcenzuweisung zur Rolle
+# entitlementValues.value - Detail des Entitlements (bei AD-Gruppen der Gruppenname)
+
+# Parent Roles (.parent.roles[])
+# (id) - technische Id der Parent Role
+# name - Name der Parent Role
+# (roleLevel.level) - Rollenebene
+# (requestDescription) - Beschreibung der Anforderung
+
+# Direkte Zuweisungen (vermutlich der Request, nicht die Zuweisung selbst)
+# (dn) - id der Rolle
+# recipientType - USER, GROUP, CONTAINER (OU)
+# recipientDn - id des Empfängers
+# (effectiveDate) - Gültigkeitsbeginn
+# (expiryDate) - Gültigkeitsende
+# (statusCode) - Statuscode der Zuweisung
+# statusDisplay - Statustext der Zuweisung (z. b. "Completed")
+# grant - Zuweisung (True), Entzug (False)
+
+
+$infos = $j | ForEach-Object {
+    $role = $_
+    @{
+        roleId            = $role.id -replace '^cn=', '' -replace ',cn=.*$', ''
+        resources         = $role.res.resources | ForEach-Object {
+            $res = $_
+            [PSCustomObject]@{
+                ressourcename       = $res.Name
+                entitlement         = $res.entitlementValues.value
+                resourceDescription = $res.description
+                mappingDescription  = $res.mappingDescription
+            }
+        }
+        parentRoles       = $role.parents.roles | ForEach-Object {
+            $parent = $_
+            [PSCustomObject]@{
+                parentRoleName     = $parent.name
+                roleLevel          = $parent.roleLevel.level
+                requestDescription = $parent.requestDescription
+            }
+        }
+        directAssignments = $role.assignments.assignmentStatusList | ForEach-Object {
+            $da = $_
+            [PSCustomObject]@{
+                recipientType = $da.recipientType
+                recipientDn   = $da.recipientDn
+                effectiveDate = $da.effectiveDate
+                expiryDate    = $da.expiryDate
+                statusCode    = $da.statusCode
+                statusDisplay = $da.statusDisplay
+                grant         = $da.grant
+            }
+        }
+    }
+}
+
+$alles = $infos | foreach-object {
+    $aktInfo = $_
+    $roleId = $aktInfo.roleId
+    $aktInfo.resources | foreach-object {
+        $aktRes = $_
+        $resourcename = $aktRes.ressourcename
+        $entitlement = $aktRes.entitlement
+        $resourceDescription = $aktRes.resourceDescription
+        $mappingDescription = $aktRes.mappingDescription
+        $aktInfo.parentRoles | foreach-object {
+            $aktParent = $_
+            $parentRoleName = $aktParent.parentRoleName
+            $roleLevel = $aktParent.roleLevel
+            $requestDescription = $aktParent.requestDescription
+            [PSCustomObject]@{
+                roleId              = $roleId
+                resourcename        = $resourcename
+                entitlement         = $entitlement
+                resourceDescription = $resourceDescription
+                mappingDescription  = $mappingDescription
+                parentRoleName      = $parentRoleName
+                parentRoleLevel     = $roleLevel
+                requestDescription  = $requestDescription
+            }
+        }
+    }
+}
---
+++ idm/misc/role-details.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const idm = require('./idm.js');
+const simulate = false;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main(fn) {
+    console.log('initializing');
+    idm.initIdm();
+    console.log('login');
+    await idm.idmLogin();
+    console.log('getting it roles');
+    let r10;
+    if (!fn) {
+        r10 = await idm.invokeIdmRest('IDMProv/rest/catalog/roles/listV2?nextIndex=1&q=*&sortOrder=asc&sortBy=name&roleLevel=10&size=7000', 'GET', {}, {}, 'roles');
+        const j10 = JSON.stringify(r10, null, 2);
+        fs.writeFileSync('/tmp/r10-raw.json', j10, { encoding: "utf8" });
+    }
+    r10 = JSON.parse(fs.readFileSync(fn, { encoding: "utf8" }));
+    const arrRoles = [];
+    console.log(`got ${r10.length} roles back, getting details ...`);
+    let blockIndex = 0;
+    let block = 0;
+    let json = '';
+    for (let ndx = 0; ndx < r10.length; ndx++) {
+        blockIndex++;
+        if (blockIndex > 200) {
+            blockIndex = 0;
+            block++;
+            json = JSON.stringify(arrRoles, null, 2);
+            fs.writeFileSync(`/tmp/r10-${block}.json`, json, { encoding: "utf8" });
+            console.log(`written new data file - /tmp/r10-${block}.json`);
+        }
+        let r = r10[ndx];
+        let id = r.id;
+        console.log(`${ndx}. Processing ${id}`);
+        let b = `{"id":"${id}"}`;
+        if (!id.toLowerCase().startsWith('cn=wurmlogin_')) {
+            if (!simulate) {
+                let res = await idm.invokeIdmRest('IDMProv/rest/catalog/roles/mappedResources/list?nextIndex=1&size=50&q=*&driverID=', 'POST', b, {}, '');
+                let parents = await idm.invokeIdmRest('IDMProv/rest/catalog/roles/parentRoles/list?size=50&q=*&nextIndex=1', 'POST', b, {}, '');
+                let assignments = await idm.invokeIdmRest('IDMProv/rest/catalog/roles/role/assignments/v2?nextIndex=1&q=&sortOrder=asc&sortBy=name&size=100', 'POST', b, {}, '');
+                arrRoles.push({ id, res, parents, assignments });
+            }
+        } else {
+            console.log(`************************** Skipping ${id} ***`);
+        }
+        await sleep(250);
+    }
+    if (!simulate) {
+        const json = JSON.stringify(arrRoles, null, 2);
+        fs.writeFileSync('/tmp/r10-final.json', json, { encoding: "utf8" });
+    }
+    await idm.idmLogout();
+}
+
+console.log('calling main');
+main('/tmp/r10-raw.json');
+console.log('finished');
